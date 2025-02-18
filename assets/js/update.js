4449683e131137f0d419d25491e3e4977d23b00c
Alexander Stämmler <staemmler.alexander@web.de>
2025-02-18 20:35:34 +0100

    Added automatic bday update script.

---
+++ assets/js/update.js
@@ -0,0 +1,10 @@
+const bannerheader = document.querySelector("#banner .content Header");
+const p = document.createElement("p");
+
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const bday = month >= 10 ? year - 1995 : year - 1996;
+
+p.innerText = "a " + bday + " year old Computer Science student from Berlin.\nI'm passionate about learning new skills to creat software and games.\nFeel free to explore my projects!";
+bannerheader.appendChild(p);
